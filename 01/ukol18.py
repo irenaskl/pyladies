fbd618e2917109bf83729838b3fc3013aba296e7
irenaskl <irsklenarova@gmail.com>
2022-09-25 19:18:31 +0200

    oprava else

--- 01/ukol18.py
+++ 01/ukol18.py
@@ -8,17 +8,18 @@ bohata = input('Jsi bohata? ')
 if (stastna == 'ano' or stastna == 'Ano'):
     if (bohata == stastna):
         print('Gratuluji!')
-if (stastna == 'ano' or stastna == 'Ano'):
-    if (bohata == 'ne' or bohata == 'Ne'):
-        print('Zkus min utracet!')
-if (stastna == 'ne' or stastna == 'Ne'):
-    if (bohata == stastna): 
-        print('To je mi lito.')
-if (stastna == 'ne' or stastna == 'Ne'): 
-    if (bohata == 'ano' or bohata == 'Ano'):    
-        print('Zkus se vice usmivat.')
-else:
-    print('Nerozumim.')
+    if (stastna == 'ano' or stastna == 'Ano'):
+        if (bohata == 'ne' or bohata == 'Ne'):
+            print('Zkus min utracet!')
+else: 
+    if (stastna == 'ne' or stastna == 'Ne'):
+        if (bohata == stastna): 
+            print('To je mi lito.')
+        if (stastna == 'ne' or stastna == 'Ne'): 
+            if (bohata == 'ano' or bohata == 'Ano'):    
+                print('Zkus se vice usmivat.')
+    else:
+        print('Nerozumim.')
 
  
 
