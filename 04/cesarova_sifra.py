dc280781811ce8a5d337550de8dc39a363bd8657
irenaskl <irsklenarova@gmail.com>
2022-10-16 18:33:08 +0200

    add modulo + while check

--- 04/cesarova_sifra.py
+++ 04/cesarova_sifra.py
@@ -3,17 +3,35 @@ from string import ascii_lowercase, ascii_uppercase
 plaintext = input('Jaky mam zasifrovat text? ')
 ciphertext = ''
 
-#while key != int():
-key = int(input('Jaky mam zadat klic? Zadej cele cislo: '))
+while True:
+    #Jestlize odpoved neni cislo, tak se dotaz opakuje
+    key = (input('Jaky mam zadat klic? Zadej cele cislo: '))
+    if key.isnumeric():
+        key = int(key)
+        break
 
 
 for pismeno in plaintext:
     #jestlize je pismeno v abecede
-    if pismeno in (ascii_lowercase[:26] + ascii_uppercase[:26]):
+    if pismeno in (ascii_uppercase[:26]):
         #prevede pismeno na znak
-        znaky = (ord(pismeno))
+        znaky = (ord(pismeno)) - (ord('A'))
+        #print(znaky)
         #prevede znak na pozadovanou sifru
-        ciphertext += (chr(znaky + key))
+        ci = (znaky + key) % 26
+        ci = (ci + (ord('A')))
+        #print(ci)
+        ciphertext += (chr(ci))
+    elif pismeno in (ascii_lowercase[:26]):
+        #prevede pismeno na znak
+        znaky = (ord(pismeno)) - (ord('a'))
+        #print(znaky)
+        #prevede znak na pozadovanou sifru
+        ci = (znaky + key) % 26
+        ci = (ci + (ord('a')))
+        #print(ci)
+        ciphertext += (chr(ci))
+
     else:
         ciphertext += pismeno
 
