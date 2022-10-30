37053107c1faf91077ece372d3820eed57d10c74
irenaskl <irsklenarova@gmail.com>
2022-10-30 19:32:22 +0100

    lesson 6

---
+++ 06/obdelnik.py
@@ -0,0 +1,8 @@
+def obsah_obdelniku(a, b):
+    if a <= 0 or b <= 0:
+        print('Invalid number')
+        return None
+    return a * b
+    
+
+print(obsah_obdelniku(2, -3))    
---
+++ 06/spocitej.py
@@ -0,0 +1,11 @@
+
+
+def spocitej(slovo):
+    delka = 0
+    for i in slovo:
+         delka += 1
+    return delka
+        
+print(spocitej('ahoj'))        
+
+        
