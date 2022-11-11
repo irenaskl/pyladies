d3858df16cbe6b8b509bea2c33899d95e9b6f33b
irenaskl <irsklenarova@gmail.com>
2022-11-11 17:25:46 +0100

    after correction

--- 07/hangman/ai.py
+++ 07/hangman/ai.py
@@ -4,10 +4,11 @@ from random import choice
 def random_word(game_list):
     '''Pocitac vybere z globalniho seznamu nahodne slovo.'''
     game_random_word = choice(game_list)  # Vybere nahodne slovo ze seznamu
-    return game_random_word               # Vyvola funkci field pro vypocet hraciho pole
+    return game_random_word  # Vyvola funkci field pro vypocet hraciho pole
 
 
 def field(game_random_word):
     '''Nastavi retezec s tolika pomlckami, kolik ma slovo.'''
-    game_field = len(game_random_word) * '-'  # Spocita pocet pismen ve slove a vypise stejny pocet '-'
+    # Spocita pocet pismen ve slove a vypise stejny pocet '-'
+    game_field = len(game_random_word) * '-'
     return game_field
--- 07/hangman/hangman.py
+++ 07/hangman/hangman.py
@@ -5,31 +5,31 @@ from string import ascii_lowercase
 def change(game_field, game_random_word):
     '''Vymeni pismeno za '-' '''
 
-    while True:      # Overeni vstupu
+    while True:  # Overeni vstupu
         letter = input('Zadej pismeno: ').lower()
-        if letter in ascii_lowercase and len(letter) == 1:      # Jestlize neni pismeno v abecede
+        # Jestlize je pismeno v abecede a ma 1 znak.
+        if letter in ascii_lowercase and len(letter) == 1:
             break
         print('Toto neni jedno pismeno!')
 
-    correct_letter = letter in game_random_word      # Pismeno je v hledanem slove
+    # Pismeno je v hledanem slove
+    correct_letter = letter in game_random_word
 
     if correct_letter:
-        position = -1  
-        for i in game_random_word:       # Vypocet pozice pismene v hracim slove
-            position += 1
-            if i == letter:
-                break
+        # Vypocet pozice pismene v hracim slove
+        position = game_random_word.index(letter)
 
-        start = game_field[:position]      # Vymena '-' za pismeno
+        start = game_field[:position]  # Vymena '-' za pismeno
         end = game_field[position + 1:]
         new_field = start + letter + end
         return result(new_field)
 
 
 def result(game_field):
-    '''Vyhodnoti stav hry'''     # Jestlize uz neni volne '-', hra skonci, v opacnem pripade hra pokracuje.
+    '''Vyhodnoti stav hry'''
+    # Jestlize uz neni volne '-', hra skonci, v opacnem pripade hra pokracuje.
     if '-' not in game_field:
         print('Gratuluji')
         exit(0)
-    else:
-        return game_field
+
+    return game_field
