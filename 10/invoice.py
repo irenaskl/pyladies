d6b04c5e9ad863b2790256761a848a7a58656e8c
irenaskl <irsklenarova@gmail.com>
2022-12-05 09:14:54 +0100

    correction

--- 10/invoice.py
+++ 10/invoice.py
@@ -10,46 +10,50 @@ URL_KURZY_CZ_API = "https://data.kurzy.cz/json/meny/b[6].json"
 INVOICE = 'template.txt'
 ACCOUNT = 'CZ2806000000000168540115'
 
+
 class CurrencyError(Exception):
+    '''Vlastni vyjimka - chyba zadane meny'''
     pass
 
 
 class Invoice:
 
-
-
     def save_file(self, data, filename) -> None:
+        '''Ulozeni url adresy do formatu .text'''
         with open(filename, 'w') as f:
             f.write(data.text)
 
 
     def download_file(self, url=URL_KURZY_CZ_API, filename=DATA_FILE):
+        '''Stazeni url adresy'''
         page = requests.get(url)
         page.raise_for_status()
         self.save_file(page, filename)
 
 
     def actual_data(self):
+        '''Kontrola aktualnosti stazene url'''
         today = date.today()
+        # Prepsani datumu do potrebneho formatu
         today = today.strftime('%Y%m%d')
-        print(today)
-        print(self.rates['den'])
         return self.rates['den'] == today
 
 
-
     def file_exists(self):
+        '''Kontrola existence souboru, do ktereho
+           se ulozi data z url'''
         file_exists = os.path.exists(DATA_FILE)
         return file_exists
 
 
     def load_data(self, filename=DATA_FILE):
-        print(filename)
+        '''Ulozeni stazenych dat do promenne'''
         with open(filename) as f:
             self.rates = json.loads(f.read())
 
 
     def count(self, amount, currency):
+        '''Vypocet konecne castky'''
         czk_currency = ['KC', 'KČ', 'CZK']
         try:
             amount = float(amount)
@@ -59,31 +63,28 @@ class Invoice:
         if amount < 0:
             raise ValueError(f'Wrong amount: {amount}')
 
-
         if currency in czk_currency:
             self.final_amount = str(amount)
-            print(self.final_amount)
         elif currency in self.rates['kurzy']:
             amount = amount * self.rates['kurzy'][currency]['dev_stred']
             self.final_amount = str(amount)
-            print(self.final_amount)
         else:
             raise CurrencyError(f'Invalid currency: {currency}')
 
 
     def variable_symbol(self):
+        '''Vygenerovani unikatniho variabilniho symbolu'''
         symbol = datetime.now()
         self.symbol = symbol.strftime('%f')
-        print(self.symbol)
 
 
     def create_invoice(self, text, file=INVOICE):
+        '''Vytvoreni faktury v html'''
         img = qrcode.make(f'SPD*1.0*ACC:{ACCOUNT}*AM:{self.final_amount}*CC:CZK*MSG:{text}*X-VS:{self.symbol}')
         image_qr = 'invoice' + self.symbol +'.png'
         img.save(image_qr)
 
-
-
+        # Vymena znaku ve fakture za znaky zadane uzivatelem
         with open(file, 'r') as f:
             template = (f.read())
             template = template.replace('AMOUNT', self.final_amount)
@@ -92,36 +93,24 @@ class Invoice:
             template = template.replace('INV', self.symbol)
             template = template.replace('ACCOUNT', ACCOUNT)
             template = template.replace('QRCODE', image_qr)
-            print(template)
+
         file2 = 'invoice' + self.symbol + '.html'
 
+        # Vytvoreni nove faktury v html
         with open(file2, 'w') as fp:
             fp.write(template)
-
-
-
-
-
-
-
-
 
 
 if __name__=='__main__':
     faktura = Invoice()
     if faktura.file_exists() == False:
-        print('vytvarim')
         faktura.download_file()
     faktura.load_data()
     if faktura.actual_data() == False:
         faktura.download_file
     amount = (input('Fill in the amount: '))
     currency = input('Fill in the currency: ').upper()
-    text = input('Platba za: ')
+    text = input('Payment for: ')
     faktura.count(amount, currency)
     faktura.variable_symbol()
     faktura.create_invoice(text)
-
-    #float(suma)
-    #faktura.vypocitaj(suma,kod)
-    #print(faktura.rates['kurzy']['AD']['dev_stred'])
